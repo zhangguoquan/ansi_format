bdcd5b3fe0228043aae41e1fc996e07d7b9430a6
zhangguoquan <zhang_guoquan@careri.com>
2018-10-12 17:05:52 +0800

    aaaa

--- ansi_format.py
+++ ansi_format.py
@@ -41,7 +41,5 @@ Button(root,bg = 'lightblue',text = "选择".decode('utf-8'), command = srcselec
 Label(root, bg = 'lightblue',text = "目的路径".decode('utf-8')).grid(row = 1, column = 0)
 Entry(root, textvariable = dstpath).grid(row = 1, column = 1)
 Button(root,bg = 'lightblue',text = "选择".decode('utf-8'), command = dstselectPath).grid(row = 1, column = 2)
-
 bttn = Button(root,text = "转换".decode('utf-8'), width = 20,bg = "lightblue",command = getallPath).grid(row = 2,column = 1)
-
 root.mainloop()
