d257ffd46fb5f5dcf3c14508d9c7825878d767b6
zhang guoquan <zhang_guoquan@careri.com>
2018-12-12 14:54:43 +0800

    去掉字符串的decode选项

--- ansi_format.py
+++ ansi_format.py
@@ -31,15 +31,15 @@ root = Tk()
 dstpath = StringVar()
 srcpath = StringVar()
 
-root.title("文件转换助手".decode('utf-8'))
+root.title("文件转换助手")
 root.geometry("240x100+400+300")      #宽 x 高 + 左边距 + 上边距
 root['bg'] =  "lightblue"
-Label(root,bg = 'lightblue', text = "源路径".decode('utf-8')).grid(row = 0, column = 0)
+Label(root,bg = 'lightblue', text = "源路径").grid(row = 0, column = 0)
 Entry(root, textvariable = srcpath).grid(row = 0, column = 1)
-Button(root,bg = 'lightblue',text = "选择".decode('utf-8'), command = srcselectPath).grid(row = 0, column = 2)
+Button(root,bg = 'lightblue',text = "选择", command = srcselectPath).grid(row = 0, column = 2)
 
-Label(root, bg = 'lightblue',text = "目的路径".decode('utf-8')).grid(row = 1, column = 0)
+Label(root, bg = 'lightblue',text = "目的路径").grid(row = 1, column = 0)
 Entry(root, textvariable = dstpath).grid(row = 1, column = 1)
-filedialog.Button(root,bg = 'lightblue',text = "选择".decode('utf-8'), command = dstselectPath).grid(row = 1, column = 2)
-filedialog.Button(root,text = "转换".decode('utf-8'), width = 20,bg = "lightblue",command = getallPath).grid(row = 2,column = 1)
+filedialog.Button(root,bg = 'lightblue',text = "选择", command = dstselectPath).grid(row = 1, column = 2)
+filedialog.Button(root,text = "转换", width = 20,bg = "lightblue",command = getallPath).grid(row = 2,column = 1)
 root.mainloop()
